6c0b877e1a1c7edaf41628a968b815c3f8623184
MissJoi89 <sjcrowder@my.waketech.edu>
2026-09-26 14:38:34 -0400

    updated magic-eight-ball.js

--- js/magic-eight-ball.js
+++ js/magic-eight-ball.js
@@ -13,56 +13,62 @@ let answers = [
 // Implement a function called displayAnswer() to do the following:
 function displayAnswer() {
 
-// Generate a random index to select a random answer from the array.
-let randomIndex = Math.floor(Math.random() * answers.length);
-let answer = answers[randomIndex];
+    // Generate a random index to select a random answer from the array.
+    let randomIndex = Math.floor(Math.random() * answers.length);
 
-// Display the selected answer in the "circle" div
-let circle = document.getElementById("circle");
+    let answer = answers[randomIndex];
 
-circle.style.display = "block";
-circle.innerHTML = answer;
+    // Display the selected answer in the "circle" div
+    let circle = document.getElementById("circle");
+
+    circle.style.display = "block";
+    circle.innerHTML = answer;
 }
 
-// Create an event listener using addEventListener()
-// for the “mousedown” event on the “ball”
-document.getElementById("ball").addEventListener("mousedown", function () {
+// Wait until the page has loaded
+document.addEventListener("DOMContentLoaded", function () {
 
-// Use an “if” statement to see if a question is typed into the question field
-// Use an “else” statement to run the displayAnswer() function
-let question = document.getElementById("question").value;
+    // Create an event listener using addEventListener()
+    // for the “mousedown” event on the “ball”
+    document.getElementById("ball").addEventListener("mousedown", function () {
 
-if (question.trim() === "") {
-alert("Please enter a yes/no question.");
-} else {
-// Display a random answer
-displayAnswer();
-}
-});
+        // Use an “if” statement to see if a question is typed into the question field
+        // Use an “else” statement to run the displayAnswer() function
+        let question = document.getElementById("question").value;
 
-// Create another event listener using addEventListener() 
-// for the “click” event on the “reset” button
-document.getElementById("reset").addEventListener("click", function () {
+        if (question.trim() === "") {
+            alert("Please enter a yes/no question.");
+        } else {
+            // Display a random answer
+            displayAnswer();
+        }
+    });
 
-// Hide the answer circle when the form is reset
-document.getElementById("circle").style.display = "none";
-});
+    // Create another event listener using addEventListener() 
+    // for the “click” event on the “reset” button
+    document.getElementById("reset").addEventListener("click", function () {
 
-// BONUS CHALLENGE
-// Use an event listener to run when the "Add New Response" button is clicked
-document.getElementById("addResponse").addEventListener("click", function () {
+    // Hide the answer circle when the form is reset
+    document.getElementById("circle").style.display = "none";
+    });
 
-    // Ask the user for a new Magic Eight Ball response
-    let newResponse = prompt("Enter a new response for the Magic Eight Ball:");
+    // BONUS CHALLENGE
+    // Use an event listener to run when the "Add New Response" button is clicked
+    document.getElementById("addResponse").addEventListener("click", function () {
 
-    // Use an if statement to see if something was entered
-    // if yes, add the new response to the answers array
-    if (newResponse && newResponse.trim() !== "") {
+        // Ask the user for a new Magic Eight Ball response
+        let newResponse = prompt("Enter a new response for the Magic Eight Ball:");
 
-        answers.push(newResponse);
+        // Use an if statement to see if something was entered
+        // if yes, add the new response to the answers array
+        if (newResponse && newResponse.trim() !== "") {
 
-        // Output both the new response added along with the current number of responses
-        console.log("New response added: " + newResponse);
-        console.log("Current number of responses: " + answers.length);
-    }
+            answers.push(newResponse);
+
+            // Output both the new response added along with the current number of responses
+            console.log("New response added: " + newResponse);
+            console.log("Current number of responses: " + answers.length);
+        }
+    });
+
 });
